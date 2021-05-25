f23d74568b12936ec3e4d12c1ceb5bbf0e4c6b33
Peter van den Hemel <petervdhemel@gmail.com>
2021-05-25 13:45:15 +0200

    Added local file writing. Changed formatted date/time to be friendly to file writing.

--- write_text/write_files.py
+++ write_text/write_files.py
@@ -2,27 +2,28 @@ from datetime import datetime
 
 dataList = []
 timeList = []
-data = ""
+inputData = ""
 dt_string = ""
 id = 0
 
-while not data == 'x':
+while not inputData == 'x':
+    '''Take input and datetime'''
     try:
-        data = input("Enter Data (x to stop): ")  # Input
+        inputData = input("Enter Data (x to stop): ")  # Input
     except ValueError:
         print("Raised ValueError, please try again.")  # ValueError Raised
         continue
     else:
-        # check if data is not x
-        if data != 'x':
+        # check if inputData is not x
+        if inputData != 'x':
             # No ValueError Raised, continue interaction
             now = datetime.now()  # get datetime
             # format datetime:
             # dd/mm/YY H:M:S
-            formatted_dt = now.strftime("%d/%m/%Y %H:%M")
+            formatted_dt = now.strftime("%d_%m_%Y_%H_%M_%S")
             if dt_string == formatted_dt:  # Check if dates are the same
                 new_string = formatted_dt
-                new_string += " "
+                new_string += "_"
                 new_string += str(id)  # Add unique ID to datetime
                 timeList.append(new_string)
                 id += 1
@@ -30,4 +31,13 @@ while not data == 'x':
                 dt_string = formatted_dt
                 timeList.append(dt_string)
 
-            dataList.append(data)
+            dataList.append(inputData)
+
+
+for i in range(len(dataList)):
+    '''Write data to individual files, named by formatted date'''
+    # Create file name from timeList
+    fileName = timeList[i]
+    fileName += ".txt"
+    with open(fileName, 'w') as file:
+        file.write(dataList[i])
